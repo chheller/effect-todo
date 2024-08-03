9302cf959762dcaf7fbd5abd4903a36fd631e66f
Charles Heller <charleshh@protonmail.com>
2024-08-02 22:51:11 -0500

    Put service into docker compose

--- src/config/server-config.ts
+++ src/config/server-config.ts
@@ -9,7 +9,7 @@ function ServerConfig([hostname, port, isDevelopment]: [
 }
 
 const serverConfig = Config.all([
-  Config.string("HOSTNAME").pipe(Config.withDefault("::1")),
+  Config.string("HOSTNAME").pipe(Config.withDefault("0.0.0.0")),
   Config.number("PORT").pipe(Config.withDefault(8080)),
   Config.boolean("IS_DEVELOPMENT").pipe(Config.withDefault(true)),
 ]);
